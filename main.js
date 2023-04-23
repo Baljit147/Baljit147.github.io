ac8957d6d53ed05658fd19bd75e21cac674f0447
Baljit Sanghera <baljit_sanghera@hotmail.ca>
2023-04-23 13:28:59 -0400

    commit for the 23rd

--- main.js
+++ main.js
@@ -1,12 +1,19 @@
 class MyHeader extends HTMLElement {
   connectedCallback() {
-    this.innerHTML = `    <div class="header">
+    this.innerHTML = `
+    <div class="header">
         <!-- Name in header -->
-        <h1>Baljit </h1>
+        <h1 id="name-h1">Baljit Sanghera</h1>
         <!-- LinkedIn logo in header -->
+        <div class="href_container">
         <a href="https://www.linkedin.com/in/baljit-sanghera-30216426b/" target="_blank">
-            <img src="LI-Logo.png" alt="LinkedIn Logo">
+            <img src="images/LI-Logo.png" alt="LinkedIn Logo" id="linkedin">
         </a>
+        <br>
+        <a href="https://github.com/Baljit147" target="_blank">
+          <img src="images/github-mark.png" id="github">
+        </a>
+        </div>
     </div>
 `;
   }
