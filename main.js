09761d395119f05e41b506862bf02211e68ae0de
Baljit Sanghera <baljit_sanghera@hotmail.ca>
2023-04-23 10:39:17 -0400

    another commit

--- main.js
+++ main.js
@@ -1,6 +1,14 @@
 class MyHeader extends HTMLElement {
   connectedCallback() {
-    this.innerHTML = `<h1>Hello world</h1>`;
+    this.innerHTML = `    <div class="header">
+        <!-- Name in header -->
+        <h1>Baljit </h1>
+        <!-- LinkedIn logo in header -->
+        <a href="https://www.linkedin.com/in/baljit-sanghera-30216426b/" target="_blank">
+            <img src="LI-Logo.png" alt="LinkedIn Logo">
+        </a>
+    </div>
+`;
   }
 }
 
