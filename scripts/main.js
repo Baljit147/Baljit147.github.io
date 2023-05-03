ee1ed3f14bdf9418759e959645e3f0c4c1d75c92
Baljit Sanghera <baljit_sanghera@hotmail.ca>
2023-05-03 10:28:34 -0400

    Update before trying to make the popular posts, about me, header, footer and follow me sections dynamic

--- scripts/main.js
+++ scripts/main.js
@@ -8,7 +8,4 @@ function copyFunction() {
   
     // Copy the text inside the text field
     navigator.clipboard.writeText(copyText.value);
-    
-    // Alert the copied text
-    alert("Copied the text: " + copyText.value);
   }
