80b6c9ff41983fa70220997319276703affb8f52
Baljit Sanghera <baljit_sanghera@hotmail.ca>
2023-05-10 10:33:11 -0400

    2nd post complete

--- scripts/main.js
+++ scripts/main.js
@@ -60,7 +60,7 @@ class MyRightColumn extends HTMLElement {
     <div class="card">
       <h3>Popular Posts</h3>
       <div class="fakeimg"><a href="https://baljit147.github.io/posts/spell_check.html">How to troubleshoot common Linux and Libre Office spell check errors</a></div><br>
-      <div class="fakeimg">Image</div><br>
+      <div class="fakeimg"><a href="https://baljit147.github.io/posts/cracking_linux_shadow_file_passwords.html">Cracking Linux Shadow File Passwords</a></div><br>
       <div class="fakeimg">Image</div>
     </div>
     <div class="card">
